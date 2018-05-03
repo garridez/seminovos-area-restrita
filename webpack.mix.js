2d1568c9b9619df36d8209f93b21af510b103241
Felipe Amaral <feliperamaral80@gmail.com>
2018-05-03 14:36:55 -0300

    Configuração inicial do tema

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,10 @@ let basePath = 'resources/assets';
 mix.js([
   'node_modules/jquery/dist/jquery.js',
   basePath + '/js/app.js'
-], 'public/js/app.js')
-	.sass('resources/assets/sass/app.scss', 'public/css');
+], 'public/js/app.js');
 
+mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.copy(basePath + '/img', 'public/img');
+
+mix.copy(basePath + '/fonts', 'public/fonts');
