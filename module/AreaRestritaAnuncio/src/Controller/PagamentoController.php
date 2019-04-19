73d23dad2d7c58f532f0b1f9de5e5dbc03f20012
Felipe Amaral <feliperamaral80@gmail.com>
2019-04-19 00:15:30 -0300

    Update PagamentoController.php

--- module/AreaRestritaAnuncio/src/Controller/PagamentoController.php
+++ module/AreaRestritaAnuncio/src/Controller/PagamentoController.php
@@ -54,7 +54,105 @@ class PagamentoController extends AbstractActionController
 
     public function processarAction()
     {
-        var_dump(__METHOD__ . ':' . __LINE__);
+        $dados = $this->params()->fromPost();
+        if (!$dados) {
+            throw new Exception('Certifique-se de que os dados foram enviados.');
+        }
+        $cadastro = $this->getCadastro();
+
+        // dados padrão
+        if (isset($dados['acao'])) {
+            $dadosPagamento['acao'] = $dados['acao'];
+        }
+        if (isset($dados['idPagamento'])) {
+            $dadosPagamento['idPagamento'] = $dados['idPagamento'];
+        }
+
+        $dadosPagamento['metodo'] = $dados['metodo'];
+        $dadosPagamento['total'] = $dados['total'];
+        $dadosPagamento['tempo_contrato'] = isset($dados['tempo_contrato']) ? $dados['tempo_contrato'] : null;
+        $dadosPagamento['idVeiculo'] = isset($dados['idVeiculo']) ? $dados['idVeiculo'] : null;
+        $dadosPagamento['idAnuncioVeiculo'] = isset($dados['idAnuncioVeiculo']) ? $dados['idAnuncioVeiculo'] : null;
+        $dadosPagamento['idCadastro'] = $cadastro['idCadastro'];
+        $dadosPagamento['idPlano'] = $dados['idPlano'];
+        $dadosPagamento['parcelas'] = 1;
+
+        // dados para pagamento Cielo
+        if ($dados['metodo'] == 'cielo') {
+            $dadosPagamento['numero_cartao'] = $dados['numero_cartao'] ?: $dados['number'];
+            $dadosPagamento['nome_cartao'] = $dados['nome_cartao'] ?: $dados['name'];
+            $dadosPagamento['validade_cartao'] = $dados['validade_cartao'] ?: $dados['expiry'];
+            $dadosPagamento['cvc_cartao'] = $dados['cvc_cartao'] ?: $dados['cvc'];
+
+            $dadosPagamento['parcelas'] = !empty($dados['parcelas']) ? $dados['parcelas'] : $dadosPagamento['parcelas'];
+            $dadosPagamento['tipo_pagamento'] = !empty($dados['tipo_pagamento']) ? $dados['tipo_pagamento'] : 'credito';
+        }
+        if (isset($_FILES) && $_FILES) {
+            $dadosPagamento['files'] = array();
+            foreach ($_FILES as $keyFile => $file) {
+
+                $cFile = curl_file_create($file['tmp_name'], $file['type'], $file['name']);
+
+                $dadosPagamento[$keyFile] = $cFile;
+            }
+        }
+
+        $routeParams = $this->params()->fromRoute();
+        $urlHelper = $this->url();
+        $getUrlRedirect = function ($action) use($urlHelper, $routeParams) {
+            $routeParams['action'] = $action;
+            return $urlHelper->fromRoute('criar-anuncio/anuncio/pagamento/metodos', $routeParams);
+        };
+
+
+
+        $response = $this->getApiClient()
+            ->pagamentosPost($dadosPagamento, null, !false)
+            ->json();
+
+        // Em caso de sucesso no pagamento
+        if (isset($response['status']) && $response['status'] == 200) {
+            $response['data']['redirect'] = true;
+
+            if (!isset($response['data']['url'])) {
+                $response['data']['url'] = $cadastro['tipoCadastro'] == 1 ? $getUrlRedirect('planorenovado') : $getUrlRedirect('concluido');
+            }
+
+            if ($dados['metodo'] == 'deposito' && $response['email_enviado']) {
+                $response['data']['url'] = $getUrlRedirect('comprovante');
+            }
+            if ($dados['metodo'] == 'deposito' && !isset($_FILES['comprovanteAnexo'])) {
+                $response['data']['url'] = $getUrlRedirect('aguardando-comprovante');
+            }
+        } else {
+            // @todo Mudar isso aqui para um sistema de log
+            $assunto = 'Erro pagamento';
+            if (isset($response['detail'])) {
+                $assunto .= ' - ' . $response['detail'];
+            }
+            $message = array();
+            $message[] = '$response = ' . var_export($response, true);
+            $message[] = '$dadosPagamento = ' . var_export($dadosPagamento, true);
+            $message[] = '$_SESSION = ' . var_export($_SESSION, true);
+            $message = '<pre>' . implode('<br><br><br>', $message) . '</pre>';
+
+            mail('projetos@seminovosbh.com.br', $assunto, $message);
+        }
+        if (isset($response['type']) && $response) {
+            switch ($response['type']) {
+                case 12001:
+                    $response['detail'] = <<<HTML
+                    <div class="text-center"><b>Aguarde!</b></div>
+                    Ainda estamos processando uma outra tentativa de pagamento.<br>
+                    O processo pode demorar de 5 a 10 minutos dependendo da sua operadora de cartão de crédito
+HTML;
+                    break;
+            }
+        }
+        echo json_encode($response);
+        die;
+//        var_dump();
+        die;
         die;
     }
 
