55dad91dc18738f28e2afe63fc4321c004884e23
Filipe Drumond <filipe.dp@outlook.com>
2019-05-09 19:31:46 -0300

    Corrigido sidebar mobile

---
+++ resources/assets/js/mvc/layout/layout/index.js
@@ -0,0 +1,21 @@
+module.exports.seletor = '.l-layout';
+
+module.exports.callback = ($) => {
+    if ($(window).width() < 992) {
+        $("body").removeClass("desktop");
+        $("body").addClass("mobile");
+    }
+    else {
+        $("body").addClass("desktop");
+        $("body").removeClass("mobile");
+    }
+    $(".toggle-sidebar").click((e) => {
+        if ($("body").hasClass("sidebar-open")) {
+            $("body").removeClass("sidebar-open");
+            $(".page-sidebar").removeClass("visible");
+        } else {
+            $("body").addClass("sidebar-open");
+            $(".page-sidebar").addClass("visible");
+        }
+    });
+};
