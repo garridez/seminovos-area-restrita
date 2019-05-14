ba75dab0b47e6c58038dd778a3f0e7f169bea492
Felipe Amaral <feliperamaral80@gmail.com>
2019-05-14 13:57:44 -0300

    Plano particular

--- module/AreaRestrita/src/Controller/AbstractActionController.php
+++ module/AreaRestrita/src/Controller/AbstractActionController.php
@@ -45,4 +45,24 @@ class AbstractActionController extends ZendAbstractActionController
                 ->get(Cadastros::class)
                 ->getCurrent();
     }
+
+    /**
+     * Retorna os dados do veículo que estiver na rota
+     */
+    public function getVeiculo()
+    {
+        $idVeiculo = (int) $this->params()->fromRoute('idVeiculo');
+
+        if (!$idVeiculo) {
+            return [];
+        }
+        $data = $this->getApiClient()->veiculosGet([
+                'ignorarCondicoesBasicas' => true
+                ], $idVeiculo)->getData();
+        if ($data) {
+            $data[0]['modeloCarro'] = $data[0]['idModelo'];
+            return $data[0];
+        }
+        return [];
+    }
 }
