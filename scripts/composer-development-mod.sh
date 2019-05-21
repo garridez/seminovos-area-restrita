1e0fe9df9109d161085feaecbc57597e176fd844
Felipe Amaral <feliperamaral80@gmail.com>
2019-05-21 17:09:46 -0300

    Desativado opcache quando em dev

--- scripts/composer-development-mod.sh
+++ scripts/composer-development-mod.sh
@@ -3,6 +3,7 @@
 
 if [ "$APPLICATION_ENV" = 'development' ]; then
     composer development-enable 
+    echo "opcache.revalidate_freq=0" >> /usr/local/etc/php/conf.d/snbh-opcache-dev.ini
 else
     composer development-disable
 fi
