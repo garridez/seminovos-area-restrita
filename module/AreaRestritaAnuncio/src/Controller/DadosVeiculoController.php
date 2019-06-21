b464d915797321b5bc24080631b866ab39f4e9e8
Felipe Amaral <feliperamaral80@gmail.com>
2019-06-21 17:44:11 -0300

    Trucate de observações

--- module/AreaRestritaAnuncio/src/Controller/DadosVeiculoController.php
+++ module/AreaRestritaAnuncio/src/Controller/DadosVeiculoController.php
@@ -76,6 +76,11 @@ class DadosVeiculoController extends AbstractActionController
 
             $data['kilometragem'] = str_replace('.', '', $data['kilometragem']);
 
+            if (isset($data['observacoes']) && $data['observacoes']) {
+                // Devido ao erro de codificação com alguns carecteres especiais, é truncado para 700
+                $data['observacoes'] = substr(utf8_decode($data['observacoes']), 0, 700);
+            }
+
             if ($idVeiculo) {
                 // Atualiza
                 $data = array_diff_key($data, array_flip([
