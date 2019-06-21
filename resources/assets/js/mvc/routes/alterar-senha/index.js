8ca250f6e6c0b9bb35796b7b977c83ae4ea054f8
Filipe Drumond Pereira <filipe.dp@outlook.com>
2019-06-21 14:50:07 -0300

    Adicionado validação form alterar senha

---
+++ resources/assets/js/mvc/routes/alterar-senha/index.js
@@ -0,0 +1,56 @@
+
+module.exports.seletor = '.c-meus-dados.a-alterar-senha';
+
+module.exports.callback = ($) => {
+    let atualSenha = $("input[name='senhaAtual']");
+    let novaSenha = $("input[name='senha']");
+    let confNovaSenha = $("input[name='confirmacaoSenha']");
+    $("input").change(function() {
+        validForm();
+    });
+    function validForm() {
+        if(novaSenha.val().length >= 4 ){
+            alterarErros(novaSenha,"Ok essa é uma senha boa","");
+            validar(novaSenha);
+            if(novaSenha.val() == confNovaSenha.val()){
+                alterarErros(confNovaSenha,"Ok essa é uma senha boa","");
+                validar(confNovaSenha);
+                return;
+            }
+        }
+        if(novaSenha.val() != atualSenha.val()){
+            alterarErros(novaSenha,"","");
+            validar(atualSenha);
+            validar(novaSenha);
+        }
+        if(novaSenha.val() == atualSenha.val()){
+            alterarErros(atualSenha,"","Sua senha nova não pode ser sua antiga senha");
+            alterarErros(novaSenha,"","Sua senha nova não pode ser sua antiga senha");
+            invalidar(atualSenha);
+            invalidar(novaSenha);
+        }
+        if(novaSenha.val() != confNovaSenha.val()){
+            alterarErros(novaSenha,"","As senhas não combinam");
+            alterarErros(confNovaSenha,"","As senhas não combinam");
+            invalidar(novaSenha);
+            invalidar(confNovaSenha);
+        }
+        if(novaSenha.val().length < 4){
+            alterarErros(novaSenha,"","Sua senha deve conter mais de 4 caractéres");
+            invalidar(novaSenha);
+        }
+    }
+    function invalidar(elemento){
+        $(elemento).removeClass("is-valid");
+        $(elemento).addClass("is-invalid");
+    }
+    function validar(elemento){
+        $(elemento).removeClass("is-invalid");
+        $(elemento).addClass("is-valid");
+    }
+    function alterarErros(elemento,textoValid,textoInvalid) {
+        let pai = $(elemento).parent();
+        $(pai).find(".invalid-feedback").text(textoInvalid);
+        $(pai).find(".valid-feedback").text(textoValid);
+    }
+};
