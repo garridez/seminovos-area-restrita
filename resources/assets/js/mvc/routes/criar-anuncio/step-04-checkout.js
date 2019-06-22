89ac87f58fcdabdc276ac86f0ac64bdb206b917f
Felipe Amaral <feliperamaral80@gmail.com>
2019-06-22 01:09:29 -0300

    QA:Tentei gerar um boleto no plano GRATIS e o mesmo da mensagem de erro. Tem que mudar o texto para: VALOR ZERADO. Selecione o plano correto para emitir o boleto.
    Solução: Ao tentar pagar um plano grátis, é redirecionado para escolher um plano

--- resources/assets/js/mvc/routes/criar-anuncio/step-04-checkout.js
+++ resources/assets/js/mvc/routes/criar-anuncio/step-04-checkout.js
@@ -1,12 +1,31 @@
 module.exports.seletor = '.c-criar-anuncio.a-index';
 
 module.exports.callback = ($) => {
-    $('.step-container')
-            .on('step:change:checkout', function (e) {
-                window.location = '#checkout';
-            })
-            .on('step:exit:checkout', function (e) {
-                window.location = '#';
-            });
+    var Alerts = require('components/Alerts');
+    var stepContainer = $('.step-container');
+    stepContainer.on('step:change:checkout', function (e) {
+        window.location = '#checkout';
+        var planoAtual = $('#dados-basicos #idPlano').val();
+        // Se for grátis vai para a tela de selecionar um plano
+        if (planoAtual === '1') {
+            stepContainer.stepPlugin('goTo', '.step-plano');
+            $('#form_Plano input[name="idPlano"][value="1"]')
+                    .prop('checked', false)
+                    .change(function (e) {
+                        if (!confirm('Quer realmente permanecer no plano grátis?')) {
+                            e.preventDefault();
+                            $(this).prop('checked', false);
+                            return false;
+                        }
+                    });
+            Alerts.info('Você está no plano <b>SIMPLES</b>.<br>'
+                    + 'Escolha um dos planos de acordo com a <b>prioridade</b> que deseja vender seu veículo<br>'
+                    + 'Lembrando que quanto mais <b>alto o plano</b>, mais seu veículo é <b>visto</b> ;)', 'Escolha um plano', 25000);
+        }
+
+
+    }).on('step:exit:checkout', function (e) {
+        window.location = '#';
+    });
 };
 
