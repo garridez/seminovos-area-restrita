8af5b592caf00b19527923dce1f5264e0b9be803
Felipe Amaral <feliperamaral80@gmail.com>
2019-07-09 15:14:15 -0300

    Adicionado script para não roda o npm quando o docker estiver rodando no windows

--- scripts/npm/development.sh
+++ scripts/npm/development.sh
@@ -5,5 +5,13 @@ if [ ! -d node_modules ]; then
     npm install
 fi
 
-echo  'npm run watch...';
-npm run watch &
+apt-update
+apt-get install iputils-ping -y
+ping -c 1 docker.for.win.localhost &>/dev/null
+
+if [ $? -eq 0 ]; then
+    echo 'Não executar o "npm run watch" quando o host for windows'
+else
+    echo  'npm run watch...';
+    npm run watch &
+fi;
