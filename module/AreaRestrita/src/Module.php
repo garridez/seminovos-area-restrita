623cdb8da6d92a4e1bbc27a4f538cf7a3dd93df2
Felipe Amaral <feliperamaral80@gmail.com>
2019-07-11 18:09:53 -0300

    Log de resposta lenta da API

--- module/AreaRestrita/src/Module.php
+++ module/AreaRestrita/src/Module.php
@@ -11,6 +11,10 @@ use Zend\Authentication\AuthenticationService as AuthService;
 use Zend\Log\Logger;
 use Zend\Mvc\MvcEvent;
 use Zend\ServiceManager\ServiceManager;
+use SnBH\ApiClient\Client as ApiClient;
+use SnBH\ApiClient\Event as ApiClientEvents;
+use Zend\EventManager\Event as ZendEvent;
+use SnBH\ApiClient\Response as ApiClientResponse;
 
 class Module
 {
@@ -25,7 +29,9 @@ class Module
     public function onBootstrap(MvcEvent $e)
     {
         $e->getApplication()->getEventManager()->attach(MvcEvent::EVENT_DISPATCH_ERROR, [$this, 'onDispatchError']);
-        $this->setLogger($e->getApplication()->getServiceManager());
+        $sm = $e->getApplication()->getServiceManager();
+        $this->setLogger($sm);
+        $this->setMeasureApiResponseTime($sm);
     }
 
     public function onDispatchError(MvcEvent $e)
@@ -42,7 +48,48 @@ class Module
         $logger = $sm->get('logger');
 
         Logger::registerErrorHandler($logger, true);
-        Logger::registerExceptionHandler($logger);
         Logger::registerFatalErrorShutdownFunction($logger);
+        if (!IS_DEV) {
+            Logger::registerExceptionHandler($logger);
+        }
+    }
+
+    public function setMeasureApiResponseTime(ServiceManager $sm)
+    {
+        /** @var Logger $logger */
+        $logger = $sm->get('logger');
+        /** @var ApiClient $apiClient */
+        $apiClient = $sm->get(ApiClient::class);
+
+        $apiClient->getEventManager()->attach(ApiClientEvents::EVENT_RESPONSE, function(ZendEvent $e) use($logger) {
+            /** @var ApiClientResponse $apiResponse */
+            $apiResponse = $e->getTarget();
+            $requestParams = $apiResponse->getRequestParams();
+            $timeRequest = $apiResponse->getTotalTime();
+            $extras = [
+                'requestParams' => [
+                    'method' => $requestParams->getMethod(),
+                    'path' => $requestParams->getPath(),
+                    'body' => $requestParams->getBody(),
+                    'useCache' => $requestParams->getUseCache(),
+                ],
+                'timeRequest' => $timeRequest,
+            ];
+            $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 10);
+            array_shift($trace); // ignore this clousure;
+            array_shift($trace); // ignore Zend\EventManager\EventManager->tiggerListeners()
+            $realCallee = [];
+            for (; $traceLine = array_shift($trace);) {
+                if (isset($traceLine['class']) && $traceLine['class'] == ApiClient::class && $traceLine['function'] == '__call') {
+                    $realCallee[] = array_shift($trace);
+                    $realCallee[] = array_shift($trace);
+                    break;
+                }
+            }
+            $extras['realCallee'] = $realCallee;
+            if ($apiResponse->getTotalTime() > 1) {
+                $logger->info("Resposta lenta da API $timeRequest segundos para '{$requestParams->getMethod()} {$requestParams->getPath()}'", $extras);
+            }
+        });
     }
 }
