2aae339b419d30082de0ac052d96f6b4a4ad5242
Felipe Amaral <feliperamaral80@gmail.com>
2019-07-24 14:10:06 -0300

    Removido https da API

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -22,7 +22,7 @@ return [
     ],
     'ApiClient' => [
         'credentials' => [
-            'serverUrl' => 'https://api2.seminovosbh.com.br',
+            'serverUrl' => 'http://api2.seminovosbh.com.br',
             'headers' => [
                 'Accept' => 'application/vnd.seminovos-bh.v1+json'
             ],
