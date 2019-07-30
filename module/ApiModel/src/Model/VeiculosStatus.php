5c03e4efc83bef15a559253db3f58f8f29a1fcf3
Felipe Amaral <feliperamaral80@gmail.com>
2019-07-30 14:18:48 -0300

    Adicionado parametro no editar fotos para veiculo ativo

---
+++ module/ApiModel/src/Model/VeiculosStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SnBH\ApiModel\Model;
+
+class VeiculosStatus extends AbstractModel
+{
+
+    const STATUS_AGUARDANDO_PAGAMENTO = 1;
+    const STATUS_ATIVO = 2;
+    const STATUS_CADASTRANDO = 3;
+    const STATUS_EXPIRADO = 4;
+    const STATUS_INATIVO = 5;
+    const STATUS_PENDENTE = 6;
+    const STATUS_REMOVIDO = 7;
+    const STATUS_VENDIDO = 8;
+    const STATUS_FOTOS_ALTERADAS = 9;
+    const STATUS_CADASTRANDO_GRATIS = 10;
+
+}
