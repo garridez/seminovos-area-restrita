0097ea12c290962e66ef49b8b899240cbcf10fdb
Felipe Amaral <feliperamaral80@gmail.com>
2019-08-09 19:27:58 -0300

    Alterado domain do cookie

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 ob_start();
 // @todo mover isso para o php.ini
 set_time_limit(180);
+ini_set('session.cookie_domain', 'seminovos.com.br');
+
 register_shutdown_function(function() {
     // Adiciona no cabeçalho da aplicação o tempo de resposta da aplicação
     if (!headers_sent()) {
