50400ce0a3b161a4c57018fd7ed2d52407d055d3
Felipe Amaral <feliperamaral80@gmail.com>
2019-08-09 14:23:01 -0300

    Corrigido comando npm run dev

--- scripts/npm/development.sh
+++ scripts/npm/development.sh
@@ -10,7 +10,7 @@ apt-get install iputils-ping -y
 ping -c 1 docker.for.win.localhost &>/dev/null
 
 if [ $? -eq 0 ]; then
-    npm run dev
+    npm run dev &
     echo 'Não executar o "npm run watch" quando o host for windows'
 else
     echo  'npm run watch...';
