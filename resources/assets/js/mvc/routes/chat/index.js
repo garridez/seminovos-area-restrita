bd18a4ffa548dd2952e9e9ab577a9960dad3ef9d
Filipe Drumond Pereira <filipe.dp@outlook.com>
2019-08-13 17:29:02 -0300

    Adicionado chat com emoji

---
+++ resources/assets/js/mvc/routes/chat/index.js
@@ -0,0 +1,23 @@
+module.exports.seletor = ".c-meus-veiculos.a-chat";
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  navigator.userAgent
+);
+module.exports.callback = $ => {
+  var jQuery = require("jquery");
+  window.jQuery = jQuery;
+  require("emojionearea");
+  window.el = $("#text_area_chat").emojioneArea({});
+  window.el[0].emojioneArea.saveEmojisAs = "shortname";
+  if (!isMobile) {
+    $(".conversation-active").removeClass("invisible");
+    return;
+  }
+  $(".chat").click(function(e) {
+    $(".conversation-active").removeClass("invisible");
+    $(".list-chats").addClass("invisible");
+  });
+  $(".chat-back").click(function(e) {
+    $(".conversation-active").addClass("invisible");
+    $(".list-chats").removeClass("invisible");
+  });
+};
