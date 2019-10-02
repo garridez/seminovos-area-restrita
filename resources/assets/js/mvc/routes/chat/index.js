bcf1c4c4c381b465a8033dcda7ac5bf4a4e47c32
Felipe Amaral <feliperamaral80@gmail.com>
2019-10-01 22:28:32 -0300

    Seletor chat

--- resources/assets/js/mvc/routes/chat/index.js
+++ resources/assets/js/mvc/routes/chat/index.js
@@ -1,4 +1,4 @@
-module.exports.seletor = ".c-meus-veiculos.a-chat";
+module.exports.seletor = ".c-chat.a-index";
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 module.exports.callback = $ => {
     import('components/Chat/init').then(function (initChat) {
