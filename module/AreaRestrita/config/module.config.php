3449ecd453b85b05757693120b67cf32596c35a6
gpenido <gilmar.penido@gmail.com>
2019-10-25 13:42:00 -0300

    teste retorno url facebook

--- module/AreaRestrita/config/module.config.php
+++ module/AreaRestrita/config/module.config.php
@@ -76,6 +76,16 @@ return [
                     ],
                 ],
             ],
+            'facebookLeads' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/facebook-leads',
+                    'defaults' => [
+                        'controller' => Controller\FacebookLeadsController::class,
+                        'action' => 'index'
+                    ]
+                ]
+            ],
             'filtros' => [
                 'type' => Literal::class,
                 'options' => [
@@ -135,6 +145,7 @@ return [
             Controller\MeusVeiculosController::class => InvokableFactory::class,
             Controller\RotaExemploController::class => InvokableFactory::class,
             Controller\TermosController::class => InvokableFactory::class,
+            Controller\FacebookLeadsController::class => InvokableFactory::class,
         ],
     ],
     'service_manager' => [
---
+++ module/AreaRestrita/src/Controller/FacebookLeadsController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace AreaRestrita\Controller;
+
+use Zend\View\Model\ViewModel;
+use Zend\Form\Element;
+use Zend\Log\Logger;
+use Zend\Log\Writer\Stream;
+
+class FacebookLeadsController extends AbstractActionController 
+{
+    public function indexAction()
+    {
+
+        if($this->getRequest()->isPost()) {
+
+            $leads = $this->getRequest()->getPost();
+            $nomeArquivo =  '/data/facebook-leads.txt';
+            $logger = new Logger();
+            $write = new Stream(getcwd() . $nomeArquivo);
+            $logger->addWriter($write);
+            $logger->info($leads);
+        }
+
+
+    }
+}
