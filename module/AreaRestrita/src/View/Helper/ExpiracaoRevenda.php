73b49f2b03df707e95c57fc6ef83c946ebe3eff0
Mateus Martins <mateusmrj@gmail.com>
2019-11-25 15:09:35 -0300

    Acerto expiração revendas

---
+++ module/AreaRestrita/src/View/Helper/ExpiracaoRevenda.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AreaRestrita\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+
+/**
+ * Esse helper é pra mostrar os dados de quantidade de anúncios usados e disponiveis na view
+ * @property date $dataExpiracaoRevenda Total de Gratuitos
+ * @property int $diasParaExpirar Total de Gratuito Publicado
+ */
+class ExpiracaoRevenda extends AbstractHelper
+{
+
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function __invoke()
+    {
+        return $this;
+    }
+
+    public function __get($name)
+    {
+        return $this->data[$name];
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+}
