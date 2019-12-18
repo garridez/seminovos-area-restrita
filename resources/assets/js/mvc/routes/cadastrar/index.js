c835e2665ddc6383d65d5cd3ac9487cd777c1b82
Mateus Martins <mateusmrj@gmail.com>
2019-12-18 11:50:58 -0300

    Verifica e-mail cadastrado

--- resources/assets/js/mvc/routes/cadastrar/index.js
+++ resources/assets/js/mvc/routes/cadastrar/index.js
@@ -5,6 +5,7 @@ module.exports.callback = ($) => {
     require('components/EstadoCidade')();
     var HandleApiError = require('components/HandleApiError');
     var Alert = require('components/Alerts');
+    var advancedAlerts = require('components/AdvancedAlerts');
 
     $('form#form_particularSite').submit(function (e) {
         e.preventDefault();
@@ -51,5 +52,46 @@ module.exports.callback = ($) => {
                 }
             }
         });
+    });
+    
+    $("form[name='form_particularSite']").find("input[name='email']").blur(function (event) {
+        var emailInput = $(this);
+        var email = emailInput.val() || '';
+
+        $("form[name='form_particularSite']").find("button")
+                        .addClass('disabled')
+                        .attr('disabled', true)
+                        .attr('title', 'Verifique os dados antes de continuar');
+        $.ajax({
+            type: "GET",
+            url: "/carro/email-disponivel/"+email,
+            dataType: "json",
+            success: function (response) {
+                emailInput
+                    .removeClass('is-invalid is-valid')
+                    .addClass(response.emailDisponivel ? 'is-valid' : 'is-invalid');
+                if (!response.emailDisponivel) {
+                    $("form[name='form_particularSite']").find("button")
+                        .addClass('disabled')
+                        .attr('disabled', true)
+                        .attr('title', 'Verifique os dados antes de continuar');
+
+                    advancedAlerts.error({
+                        title: "E-mail já cadastrado",
+                        text: "E-mail já cadastrado no sistema, confira o e-mail ou entre em contato.",
+                        time: 10000
+                    })
+                    return;
+                }
+                $("form[name='form_particularSite']").find("button")
+                    .removeClass('disabled')
+                    .attr('disabled', false)
+                    .attr('title', 'Continuar');
+            },
+            error: function (e) {
+
+            }
+        });
+
     });
 };
