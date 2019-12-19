125f239197828a76535276a7792485310e505e19
Filipe Drumond Pereira <filipe.dp@outlook.com>
2019-12-19 11:48:46 -0300

    Feito a View do painel

---
+++ resources/assets/js/mvc/routes/painel/index.js
@@ -0,0 +1,45 @@
+
+
+module.exports.seletor = '.c-painel.a-index';
+module.exports.callback = ($) => {
+    let datepicker = require('js-datepicker');
+    $(".input").mask("00/00/0000");
+    let picker = datepicker(".input", {
+        customDays: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+        customMonths: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        overlayButton: "Enviar",
+        overlayPlaceholder: 'Escolha um ano 4 dig',
+        formatter: (input, date, instance) => {
+            const value = date.toLocaleDateString()
+            input.value = value // => '1/1/2099'
+        }
+    });
+    var Chart = require('chart.js');
+    var ctx = document.getElementById('myChart');
+    var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Nitro', 'Turbo', 'Básico'],
+            datasets: [{
+                label: 'Planos',
+                data: [12, 19, 3],
+                backgroundColor: [
+                    'rgba(167, 39, 18, 1)',
+                    'rgba(237, 153, 2, 1)',
+                    'rgba(241, 209, 22, 1)'
+                ],
+                // borderColor: [
+                //     'rgba(167, 39, 18, 1)',
+                //     'rgba(237, 153, 2, 1)',
+                //     'rgba(241, 209, 22, 1)'
+                // ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            legend: {
+                position: "right",
+            }
+        }
+    });
+};
