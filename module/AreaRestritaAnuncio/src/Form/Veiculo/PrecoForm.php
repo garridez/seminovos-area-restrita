be10fe32a048dcf56643ab9663b3bea3fd4eee0b
Gilmar Penido <gilmar.penido@gmail.com>
2020-01-16 11:40:34 -0300

    flag leilao

--- module/AreaRestritaAnuncio/src/Form/Veiculo/PrecoForm.php
+++ module/AreaRestritaAnuncio/src/Form/Veiculo/PrecoForm.php
@@ -59,6 +59,16 @@ class PrecoForm extends Form
             ],
         ]);
         $this->add([
+            'type' => Element\Checkbox::class,
+            'name' => 'flagLeilao',
+            'options' => [
+                'label' => 'Carro proveniente de leilão?',
+                'use_hidden_element' => true,
+                'checked_value' => 1,
+                'unchecked_value' => 0,
+            ],
+        ]);
+        $this->add([
             'type' => Element\Submit::class,
             'name' => 'submit',
             'attributes' => [
