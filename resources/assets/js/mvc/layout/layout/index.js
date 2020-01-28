3bfd948315b5fcc0f50b2e46803c8d94f4b19fc0
Felipe Amaral <feliperamaral80@gmail.com>
2020-01-28 19:36:12 -0300

    Formatado

--- resources/assets/js/mvc/layout/layout/index.js
+++ resources/assets/js/mvc/layout/layout/index.js
@@ -1,30 +1,30 @@
 module.exports.seletor = ".l-layout";
 
 module.exports.callback = $ => {
-  require("bootstrap/js/dist/util.js");
-  require("bootstrap/js/dist/collapse");
-  require("bootstrap/js/dist/dropdown");
-  require("components/pagesQuickView")($);
-  require("components/pagesMobileView")($);
-  require("components/pagesChat")($);
+    require("bootstrap/js/dist/util.js");
+    require("bootstrap/js/dist/collapse");
+    require("bootstrap/js/dist/dropdown");
+    require("components/pagesQuickView")($);
+    require("components/pagesMobileView")($);
+    require("components/pagesChat")($);
 
-  if ($(window).width() < 992) {
-    $("body").removeClass("desktop");
-    $("body").addClass("mobile");
-  } else {
-    $("body").addClass("desktop");
-    $("body").removeClass("mobile");
-  }
-  $(".toggle-sidebar").click(e => {
-    if ($("body").hasClass("sidebar-open")) {
-      $("body").removeClass("sidebar-open");
-      $(".page-sidebar").removeClass("visible");
+    if ($(window).width() < 992) {
+        $("body").removeClass("desktop");
+        $("body").addClass("mobile");
     } else {
-      $("body").addClass("sidebar-open");
-      $(".page-sidebar").addClass("visible");
+        $("body").addClass("desktop");
+        $("body").removeClass("mobile");
     }
-  });
+    $(".toggle-sidebar").click(e => {
+        if ($("body").hasClass("sidebar-open")) {
+            $("body").removeClass("sidebar-open");
+            $(".page-sidebar").removeClass("visible");
+        } else {
+            $("body").addClass("sidebar-open");
+            $(".page-sidebar").addClass("visible");
+        }
+    });
 
-  let menu = $(".menu-items");
-  menu.find();
+    let menu = $(".menu-items");
+    menu.find();
 };
