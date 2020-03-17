c15c7547acfa9366cce6349d1ffceceac86b7a89
Filipe Drumond Pereira <filipe.dp@outlook.com>
2020-03-17 14:17:59 -0300

    CORRIGIDO URL DO WEBSOCKET

--- resources/websocket/index.js
+++ resources/websocket/index.js
@@ -7,7 +7,7 @@ const httpServer = HttpServer();
 
 const apiClient = (function () {
     let serverUrl = process.env.SNBH_API_HOST || 'http://localhost:8081';
-    // serverUrl = 'http://dev.seminovos.com.br:8081/';
+    serverUrl = 'http://snbh-api';
     serverUrl = 'http://api2.seminovosbh.com.br';
 
     let headers = {};
