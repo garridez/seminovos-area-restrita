dbbe62469b38ef521c00a9e653d1fb630c8bbbfc
Filipe Drumond Pereira <filipe.dp@outlook.com>
2020-05-08 12:43:59 -0300

    seções de conteúdo central de informações

--- resources/assets/js/mvc/routes/pages/central-informacoes.js
+++ resources/assets/js/mvc/routes/pages/central-informacoes.js
@@ -2,5 +2,26 @@
 module.exports.seletor = '.c-pages.a-central-informacoes';
 
 module.exports.callback = ($) => {
+  var animationTime = 200;
+  $('body').on('click', '.opcoes .opcao', function () {
+    var target = $(this).data('target');
+    if (!target) return;
+    var $target = $(target);
 
+
+    var action = 'open';
+    if ($target.hasClass('collapsed')) action = 'close';
+
+    $('.content >.conteudo >div').slideUp(animationTime).addClass('collapsed');
+
+    if(action == 'close'){
+      $target.removeClass('collapsed');
+      $target.slideDown(animationTime);
+      return;
+    }
+
+    $target.addClass('collapsed');
+    $target.slideUp(animationTime);
+    return;
+});
 };
