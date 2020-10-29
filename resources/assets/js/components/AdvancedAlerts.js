f54185f0d4f3474ed333283e4a58100ceda1b91a
Filipe Drumond Pereira <filipe.dp@outlook.com>
2020-10-29 18:42:47 -0300

    melhoria alert botão fechar

--- resources/assets/js/components/AdvancedAlerts.js
+++ resources/assets/js/components/AdvancedAlerts.js
@@ -16,7 +16,7 @@ module.exports = advancedAlerts = {
     alert: function (type, options) {
         options = $.extend({}, this.optionsDefault, options);
         var close = $('<button class="btn btn-primary" data-dismiss="modal">')
-            .html(`<span class="text-close">${options.closeText}</span>`).click(function () { });
+            .html(`<span class="text-close">${options.closeText} </span>`).click(function () { });
 
         if (typeof options.img === "string" && options.img !== "") {
             options.img = $(`<img src="${options.img}" class="modal-img">`)
