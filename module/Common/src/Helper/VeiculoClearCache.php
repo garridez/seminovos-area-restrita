5b29c621a4319d2f316e82bb0a4afbb6d02c6875
Felipe Amaral <feliperamaral80@gmail.com>
2023-04-12 11:02:08 +0100

    Corrigido clear cache para busca por placa

--- module/Common/src/Helper/VeiculoClearCache.php
+++ module/Common/src/Helper/VeiculoClearCache.php
@@ -2,6 +2,8 @@
 
 namespace SnBH\Common\Helper;
 
+use SnBH\ApiClient\Client as ApiClient;
+
 class VeiculoClearCache
 {
 
@@ -12,8 +14,17 @@ class VeiculoClearCache
             $host = 'https://seminovos.com.br';
         }
         $url = "{$host}/{$idVeiculo}?clear-cache=1";
-        $url = "https://seminovos.com.br/{$idVeiculo}?clear-cache=1";
         @file_get_contents($url);
-        
+
+        /** @var \Zend\ServiceManager\ServiceManager $sm */
+        global $sm;
+        /** @var ApiClient $apiClient */
+        $apiClient = $sm->get(ApiClient::class);
+        $data = $apiClient->veiculosGet(['ignorarCondicoesBasicas' => 1], $idVeiculo) ->getData();
+        if (!$data) {
+            return;
+        }
+
+        @file_get_contents("{$host}/veiculo-placa?consultaPlaca=true&placaId={$data[0]['placa']}&clear-cache=1");
     }
 }
