b47952eb1ca7d6d3a63779244cc0d429d4aba8c0
Felipe Amaral <feliperamaral80@gmail.com>
2023-04-25 04:23:07 +0100

    Atualizações de código para PHP 8.1

--- rector.php
+++ rector.php
@@ -30,7 +30,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_80,
+        LevelSetList::UP_TO_PHP_81,
         //SetList::CODE_QUALITY,
     ]);
 
