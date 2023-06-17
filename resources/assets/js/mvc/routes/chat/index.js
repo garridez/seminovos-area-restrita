1b6805871b9bde567227072829e1fc70eab00beb
Felipe Amaral <feliperamaral80@gmail.com>
2023-06-17 15:19:41 -0300

    Removido arquivos do chat

--- resources/assets/js/mvc/routes/chat/index.js
+++ resources/assets/js/mvc/routes/chat/index.js
@@ -1,7 +1,5 @@
 module.exports.seletor = ".c-chat.a-index";
 var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 module.exports.callback = $ => {
-    import('components/Chat/init').then(function (initChat) {
-        initChat.default($('.chat-root'));
-    });
+
 };
