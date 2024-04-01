4f14c85c6eff7604531993ccb9de2e5e21b4e766
Fernando <fb.ciriaco@bitbucket.com>
2024-04-01 11:28:37 -0300

    Fix arquivos js

--- resources/assets/js/mvc/routes/repasse/anuncio.js
+++ resources/assets/js/mvc/routes/repasse/anuncio.js
@@ -1,4 +1,6 @@
-$("input[name='license-plate']").keyup(function(){
+module.exports.seletor = '.c-repasse.a-anuncio';
+module.exports.callback = async ($) => {
+  $("input[name='license-plate']").keyup(function(){
     licensePlate = $("input[name='license-plate']").val();
     if(licensePlate.length == 7){
         var settings = {
@@ -29,48 +31,47 @@ $("input[name='license-plate']").keyup(function(){
           }
         });
     }
-})
+});
 
-$(document).ready(function(){
-
-    $("body").on('submit', '#save', function(e){
+$("body").on('submit', '#save', function(e){
         
-        e.preventDefault();
-        var form = document.getElementById('save');
-    
-        var formData = new FormData(form);
-        formData.append("email", email);
-        formData.append("license_plate", $("input[name='license-plate']").val());
-        formData.append("creation_year", anoFabrica);
-        formData.append("model_year", anoModelo);
-        formData.append("fipe_price", numeroFipe);
-        formData.append("courier_value_sale", $("input[name='valor']").val());
-        formData.append("courier_brand", marca);
-        formData.append("courier_model", modelo);
-        formData.append("courier_description", $("textarea[name='description']").val());
-        let fileInput = document.querySelector('input[type="file"]');
-        if (fileInput.files.length > 0) {
-            for (let i = 0; i < fileInput.files.length; i++){
-                formData.append(`pictures[${i}]`, fileInput.files[i]);
-            }
-        }
-        
-        var settingsPost = {
-          "url": "https://autoconecta.com.br/api/vehicles",
-          "method": "POST",
-          "timeout": 0,
-          "headers": {
-            "Accept": "application/json",
-          },
-          "processData": false,
-          "mimeType": "multipart/form-data",
-          "contentType": false,
-          "data": formData
-        };
+  e.preventDefault();
+  var form = document.getElementById('save');
 
-        $.ajax(settingsPost).done(function (response) {
-          window.location.href = '/repasse'
-        });
+  var formData = new FormData(form);
+  formData.append("email", email);
+  formData.append("license_plate", $("input[name='license-plate']").val());
+  formData.append("creation_year", anoFabrica);
+  formData.append("model_year", anoModelo);
+  formData.append("fipe_price", numeroFipe);
+  formData.append("courier_value_sale", $("input[name='valor']").val());
+  formData.append("courier_brand", marca);
+  formData.append("courier_model", modelo);
+  formData.append("courier_description", $("textarea[name='description']").val());
+  let fileInput = document.querySelector('input[type="file"]');
+  if (fileInput.files.length > 0) {
+      for (let i = 0; i < fileInput.files.length; i++){
+          formData.append(`pictures[${i}]`, fileInput.files[i]);
+      }
+  }
+  
+  var settingsPost = {
+    "url": "https://autoconecta.com.br/api/vehicles",
+    "method": "POST",
+    "timeout": 0,
+    "headers": {
+      "Accept": "application/json",
+    },
+    "processData": false,
+    "mimeType": "multipart/form-data",
+    "contentType": false,
+    "data": formData
+  };
 
-})    
-})
+  $.ajax(settingsPost).done(function (response) {
+    window.location.href = '/repasse'
+  });
+
+});
+}
+
