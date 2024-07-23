b47f816ef3cc7798333f4884a6d8c9933fd15d04
Felipe Amaral <feliperamaral80@gmail.com>
2024-07-23 10:05:53 -0300

    Upload de multiplas imagens quando vazio.

--- resources/assets/js/mvc/routes/criar-anuncio/step-03-fotos.ts
+++ resources/assets/js/mvc/routes/criar-anuncio/step-03-fotos.ts
@@ -3,32 +3,32 @@ import Compress from 'compress.js';
 
 export const seletor = '.c-criar-anuncio.a-index';
 
-export const callback = ($) => {
+export const callback = ($: JQueryStatic) => {
     $('.step-container').on('steps-loaded', init);
 
-    var DataLayerGTMPopulate = require('../../../helpers/DataLayerGTMPopulate');
+    const DataLayerGTMPopulate = require('../../../helpers/DataLayerGTMPopulate');
     $('.step-container').on('step:pre-exit:video', function () {
         if ($('#dados-basicos #flagCriando').val() == 1) {
-            var ctx = $('.step-0, .step-1');
+            const ctx = $('.step-0, .step-1');
             DataLayerGTMPopulate(ctx, 'checkout_step_5');
         }
     });
     $('.step-container').on('step:pre-exit:plano', function () {
         if ($('#dados-basicos #flagCriando').val() == 1) {
-            var ctx = $('.step-0, .step-1');
+            const ctx = $('.step-0, .step-1');
             DataLayerGTMPopulate(ctx, 'checkout_step_6');
         }
     });
 };
 function init() {
-    var ctx = $('.step-fotos');
+    const ctx = $('.step-fotos');
     if (!ctx.length) {
         return;
     }
 
-    var rotate = ['rotate(0deg)', 'rotate(90deg)', 'rotate(180deg)', 'rotate(270deg)'];
+    const rotate = ['rotate(0deg)', 'rotate(90deg)', 'rotate(180deg)', 'rotate(270deg)'];
 
-    var handle = '.btn-move';
+    let handle = '.btn-move';
     if (
         /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     ) {
@@ -43,30 +43,49 @@ function init() {
         },
     });
 
-    var inputFoto = ctx.find('[name="foto"]');
+    const inputFoto = ctx.find<HTMLInputElement>('[name="foto"]');
+    const inputFotosMulti = ctx.find<HTMLInputElement>('[name="fotos"]');
 
     ctx.find('.fotos-container').on('click', '.display-img', function (e) {
         e.preventDefault();
+
+        const totalImgs = $('.display-img').length;
+        const totalImgsEmpty = $('.display-img').filter((i, e) => {
+            return !$(e).data('file-data') && !$(e).data('idfoto');
+        }).length;
+
+        if (totalImgs === totalImgsEmpty) {
+            inputFotosMulti[0].click();
+            return false;
+        }
+
         inputFoto.data('img-element', this);
         inputFoto[0].click();
         return false;
     });
-    inputFoto.change(function () {
-        showPhoto($(this).data('img-element'), this.files[0]);
+    inputFoto.on('change', function () {
+        if (this.files) {
+            showPhoto($(this).data('img-element'), this.files[0]);
+        }
         //compressPhoto($(this).data('img-element'), this.files[0]);
     });
-    ctx.find('[name="fotos"]') // Input de multiplos arquivos
-        .change(function () {
-            var imgs = $('.fotos-container .display-img');
-            // Reseta as imagens
-            imgs.each(function () {
-                $(this).attr('src', $(this).data('placeholder'));
-            });
-            $(this.files).each(function (i, file) {
+    // Input de multiplos arquivos
+    inputFotosMulti.on('change', function () {
+        const imgs = $('.fotos-container .display-img');
+        // Reseta as imagens
+        imgs.each(function () {
+            $(this).attr('src', $(this).data('placeholder'));
+        });
+
+        const files = this.files;
+        if (files) {
+            const arr = Array.from(files);
+            arr.forEach(function (file, i) {
                 showPhoto(imgs.eq(i), file);
                 //compressPhoto(imgs.eq(i), file);
             });
-        });
+        }
+    });
 
     ctx.on('click', '.btn-upload-img', function (e) {
         e.preventDefault();
@@ -76,7 +95,7 @@ function init() {
     ctx.on('click', '.btn-remove-img', function (e) {
         e.preventDefault();
         // Seta o placeholder e limpa os metadados
-        var img = $(this).closest('.foto').find('.display-img');
+        const img = $(this).closest('.foto').find('.display-img');
 
         //oculta os botões de ação
         $(this).closest('.foto').find('.controls').addClass('d-none');
@@ -87,8 +106,8 @@ function init() {
     });
     ctx.on('click', '.btn-to-rotate', function (e) {
         e.preventDefault();
-        var imagem = $(this).closest('.foto').find('.display-img');
-        var posicaoRotacao = imagem.data('posicao-rotacao');
+        const imagem = $(this).closest('.foto').find('.display-img');
+        let posicaoRotacao = imagem.data('posicao-rotacao');
 
         // Seta o numero de vezes que a imagem vai ser rotacionada em 90graus
         if (posicaoRotacao === 3) {
@@ -120,11 +139,11 @@ function init() {
      *      Se não for passado, então é colocado no lugar o placeholder
      * @return void
      */
-    async function showPhoto(imgElement, file) {
+    async function showPhoto(imgElement: JQuery<HTMLElement>, file?: string | File) {
         imgElement = $(imgElement);
 
-        function setBackgroudImage(file) {
-            var background = 'url("' + file + '")';
+        function setBackgroudImage(file: string) {
+            const background = 'url("' + file + '")';
 
             if (imgElement.css('background-image') === background) {
                 return;
@@ -160,7 +179,7 @@ function init() {
             return;
         }
 
-        var fileDataUrl = await fileReaderPromise(file);
+        const fileDataUrl = await fileReaderPromise(file);
         if (typeof fileDataUrl !== 'string') {
             throw new Error('Não foi possível ler o arquivo');
         }
@@ -176,44 +195,50 @@ function init() {
 
         imgElement.data('file-data', file).data('uploaded', false);
 
-        var fileCompressed = await compressPhoto(file);
+        const fileCompressed = await compressPhoto(file);
 
         if (fileCompressed) {
             imgElement.data('file-data', fileCompressed);
-            let fileDataUrl = await fileReaderPromise(fileCompressed);
-            setBackgroudImage(fileDataUrl);
+            const fileDataUrl = await fileReaderPromise(fileCompressed);
+            if (fileDataUrl) {
+                setBackgroudImage(fileDataUrl);
+            }
         }
 
-        var eventName = 'fotos:selecionada';
-        var event = $.Event(eventName);
-        event.target = imgElement.get(0);
+        const eventName = 'fotos:selecionada';
+        const event = $.Event(eventName);
+        //event.target = imgElement.get(0);
         imgElement.trigger(eventName).closest('.fotos-container').trigger(event);
     }
-    async function fileReaderPromise(file) {
-        return new Promise(function (resolve) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                resolve(e.target.result);
+    async function fileReaderPromise(file: File) {
+        return new Promise<string | null>(function (resolve) {
+            const reader = new FileReader();
+            reader.onload = function (e: ProgressEvent<FileReader>) {
+                if (e.target) {
+                    resolve(e.target.result as string | null);
+                }
             };
             reader.readAsDataURL(file);
         });
     }
-    async function compressPhoto(imageFile) {
+    async function compressPhoto(imageFile: File) {
         /**
          * Se for o Safari, não comprime
          * O Safari dá pau na hora de colocar a imagem gerada no FormData
          */
-        if (navigator.userAgent.match(/Version\/[\d\.]+.*Safari/)) {
+        if (navigator.userAgent.match(/Version\/[\d.]+.*Safari/)) {
             return false;
         }
 
-        return new Promise(function (resolve) {
+        return new Promise<File | false>(function (resolve) {
             (async () => {
                 // Se o compress falhar, não tem problema. A imagem original já está settada para enviar
                 try {
                     if (imageFile.name.match(/.heic$/) !== null) {
-                        var heic2any = require('../../../components/heic2any');
-                        var resultBlob = await heic2any({
+                        // eslint-disable-next-line @typescript-eslint/no-var-requires
+                        const heic2any = require('heic2any');
+                        //const heic2any = require('../../../components/heic2any');
+                        const resultBlob = await heic2any({
                             blob: imageFile,
                             toType: 'image/jpg',
                         });
@@ -222,7 +247,7 @@ function init() {
                             lastModified: new Date().getTime(),
                         });
                     }
-                    var compress = new Compress();
+                    const compress = new Compress();
 
                     compress
                         .compress([imageFile], {
@@ -233,22 +258,22 @@ function init() {
                             maxHeight: 750,
                         })
                         .then(function (images) {
-                            var img = images[0];
-                            function dataURLtoFile(dataurl, filename) {
-                                var arr = dataurl.split(','),
-                                    mime = arr[0].match(/:(.*?);/)[1],
-                                    bstr = atob(arr[1]),
-                                    n = bstr.length,
-                                    u8arr = new Uint8Array(n);
+                            const img = images[0];
+                            function dataURLtoFile(dataurl: string, filename: string) {
+                                const arr = dataurl.split(',');
+                                const mime = (arr[0].match(/:(.*?);/) || [])[1];
+                                const bstr = atob(arr[1]);
+                                let n = bstr.length;
+                                const u8arr = new Uint8Array(n);
                                 while (n--) {
                                     u8arr[n] = bstr.charCodeAt(n);
                                 }
                                 return new File([u8arr], filename, { type: mime });
                             }
 
-                            var file = dataURLtoFile(img.prefix + img.data, 'min_' + img.alt);
+                            const file = dataURLtoFile(img.prefix + img.data, 'min_' + img.alt);
                             resolve(file);
-                            var nF = new Intl.NumberFormat('pt-BR', {
+                            const nF = new Intl.NumberFormat('pt-BR', {
                                 maximumFractionDigits: 2,
                             }).format;
                             /** Debug * /
@@ -271,14 +296,14 @@ function init() {
 }
 /* eslint @typescript-eslint/no-unused-vars: 0 */
 function testImgUpload() {
-    var inputFoto = $('.step-fotos').find('[name="foto"]');
+    const inputFoto = $('.step-fotos').find<HTMLInputElement>('[name="foto"]');
     if (location.href.indexOf('insert') !== -1) {
         setTimeout(async function () {
-            var list = [1, 2, 3, 4, 5];
+            const list = [1, 2, 3, 4, 5];
 
-            for (var key in list) {
-                var i = (list[key] + '').padStart(2, 0);
-                var url = `/sequenciais/${i}.jpg`;
+            for (const key in list) {
+                const i = (list[key] + '').padStart(2, '0');
+                const url = `/sequenciais/${i}.jpg`;
                 await loadURLToInputFiled(inputFoto[0], url);
                 inputFoto.data('img-element', $('.display-img')[key]);
                 $('[type="file"][name="foto"]').change();
@@ -288,30 +313,28 @@ function testImgUpload() {
             }, 100);
 
             return;
-            console.log($('[type="file"][name="foto"]')[0].files);
-            $('[type="file"][name="foto"]').change();
+            //console.log($('[type="file"][name="foto"]')[0].files);
+            //$('[type="file"][name="foto"]').change();
         }, 1000);
     }
 
-    async function loadURLToInputFiled(input, url) {
+    async function loadURLToInputFiled(input: HTMLInputElement, url: string) {
         return new Promise(function (resolve) {
-            getImgURL(url, (imgBlob) => {
-                let fileName = 'hasFilename.jpg';
-                let file = new File(
-                    [imgBlob],
-                    fileName,
-                    { type: 'image/jpeg', lastModified: new Date().getTime() },
-                    'utf-8',
-                );
-                let container = new DataTransfer();
+            getImgURL(url, (imgBlob: BlobPart) => {
+                const fileName = 'hasFilename.jpg';
+                const file = new File([imgBlob], fileName, {
+                    type: 'image/jpeg',
+                    lastModified: new Date().getTime(),
+                });
+                const container = new DataTransfer();
                 container.items.add(file);
                 input.files = container.files;
-                resolve();
+                resolve(true);
             });
         });
     }
-    function getImgURL(url, callback) {
-        var xhr = new XMLHttpRequest();
+    function getImgURL(url: string, callback: (e: BlobPart) => void) {
+        const xhr = new XMLHttpRequest();
         xhr.onload = function () {
             callback(xhr.response);
         };
